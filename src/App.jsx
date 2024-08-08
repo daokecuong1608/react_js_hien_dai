aa491cfdc7c7762f10146299fc75c1daf798cbe9
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-08-08 16:36:40 +0700

    form mik va react form hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer/Footer';
 import Lesson3 from './lessons/Lesson3/Lesson3';
 import Lesson4 from './lessons/Lesson4/Lesson4';
 import Lesson5 from './lessons/Lesson5/Lesson5';
+import Lesson6 from './lessons/Lesson6/Lesson6';
 
 
 const App = () => {
@@ -20,7 +21,7 @@ const App = () => {
       /> */}
       {/* <Lesson3 /> */}
       {/* <Lesson4 /> */}
-      <Lesson5 />
+      <Lesson6 />
        
       <Footer />
     </div>
