8c880aa9984ada6d6ac3a720de82cd1c252d36fb
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-08-18 17:11:31 +0700

    normal assign

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -3,6 +3,8 @@ import "./Project.scss";
 // import "nprogress/nprogress.css";
 import Lesson17 from "./chapter3/Lesson17";
 import Lesson18 from "./chapter3/Lesson18";
+import Lesson19 from "./chapter3/Lesson19";
+import Lesson20 from "./chapter3/Lesson20";
 
 const Project = (props) => {
   return (
@@ -10,7 +12,9 @@ const Project = (props) => {
       <Login />
       {/* <Lesson26 /> */}
       {/* <Lesson17 /> */}
-      <Lesson18 />
+      {/* <Lesson18 /> */}
+      {/* <Lesson19 /> */}
+      <Lesson20 />
     </div>
   );
 };
