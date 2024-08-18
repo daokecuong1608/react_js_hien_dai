129739a3aa4eb1d76c43ba2078ef4350aa5b6046
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-08-18 16:57:38 +0700

    boolean

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -2,13 +2,15 @@ import Login from "./Login";
 import "./Project.scss";
 // import "nprogress/nprogress.css";
 import Lesson17 from "./chapter3/Lesson17";
+import Lesson18 from "./chapter3/Lesson18";
 
 const Project = (props) => {
   return (
     <div className="project">
       <Login />
       {/* <Lesson26 /> */}
-      <Lesson17 />
+      {/* <Lesson17 /> */}
+      <Lesson18 />
     </div>
   );
 };
