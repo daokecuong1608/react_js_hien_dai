311ea110488d6648dea97ef2d3fc7c35f459d2ea
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-09-06 16:22:12 +0700

    useImmer

--- src/project/chapter4/lesson35/Lesson35.jsx
+++ src/project/chapter4/lesson35/Lesson35.jsx
@@ -1,12 +1,12 @@
 import QuizQA from "./QA/QuizQA";
 
 const Lesson35 = (props) => {
-    return (
-        <div>
-            <div>Lesson 35:</div>
-            <QuizQA />
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>Lesson 35:</div>
+      <QuizQA />
+    </div>
+  );
+};
 
 export default Lesson35;
